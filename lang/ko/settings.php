3dd27845f3929dc53d6c64b037c4c87f71089438
Myeongjin <aranet100@gmail.com>
2013-12-18 14:35:55 +0900

    Add Korean translation

---
+++ lang/ko/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Korean language file for the flattr plugin
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+
+$lang['default_uid'] = '주어진 것이 없으면 사용할 기본 flattr UID.';
+$lang['default_category'] = '주어진 것이 없으면 사용할 기본 분류.';
+$lang['default_language'] = '주어진 것이 없으면 선택할 기본 언어.';
